6d3a97b97317aadee6f49359c0b9b0a9aafb7d0e
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-02-04 20:31:26 +0500

    Update App.js
    set path for home page using react router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import Home from "./pages/Home";
 import NavBar from "./components/Common/NavBar";
 import OurVendor from "./components/Common/OurVendor";
 import Footer from "./components/Common/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <OurVendor />
       <Footer />
-    </>
+    </Router>
   );
 }
 
